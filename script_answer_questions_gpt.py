32d26c15afa6e2b103e2fd58e006b453b42b8df0
Luca Benedetto <luca.benedetto93@gmail.com>
2023-11-03 10:51:22 +0000

    refactor script to use updates methods and constants

--- script_answer_questions_gpt.py
+++ script_answer_questions_gpt.py
@@ -3,9 +3,9 @@ import os
 import pandas as pd
 import openai
 
-from utils import get_student_levels_from_prompt_idx
-from utils_openai_api import prepare_answers_dict
-from constants import RACE, ARC, DATA_DIR
+from utils import get_student_levels_from_prompt_idx, get_dataset
+from utils_openai_api import prepare_answers_dict_gpt
+from constants import RACE, ARC, DATA_DIR, IS_READING_QUESTION, OUTPUT_DIR
 
 # get the OpenAI API key
 with open('key.json', 'r') as f:
@@ -19,21 +19,16 @@ DATASET = RACE
 PROMPT_IDX = 47
 
 st_levels = get_student_levels_from_prompt_idx(PROMPT_IDX)
+df_items = get_dataset(DATASET, 50)
 
-if DATASET == RACE:
-    df_items = pd.read_csv(os.path.join(DATA_DIR, "processed/race_pp_test_50q_per_diff.csv"))
-    out_dir = 'output/2309_gpt_responses_race_pp'
-    is_reading_question = True
-elif DATASET == ARC:
-    df_items = pd.read_csv(os.path.join(DATA_DIR, "processed/arc_test_50q_per_diff.csv"))
-    out_dir = 'output/23_07_gpt_responses'
-    is_reading_question = False
-else:
-    raise NotImplementedError
+is_reading_question = IS_READING_QUESTION[DATASET]
+
+# folder_name = '2309_gpt_responses_race_pp'  # RACE
+folder_name = '23_07_gpt_responses'  # ARC
 
 for idx, student_level in enumerate(st_levels):
     print(f"-- Doing idx {idx}, student level {student_level}")
-    answers_dict = prepare_answers_dict(df_items, model, student_level, is_reading_question=is_reading_question, prompt_idx=PROMPT_IDX)
+    answers_dict = prepare_answers_dict_gpt(df_items, model, student_level, is_reading_question=is_reading_question, prompt_idx=PROMPT_IDX)
 
     rows = []
     for item, value in answers_dict.items():
@@ -45,6 +40,6 @@ for idx, student_level in enumerate(st_levels):
     df_model_answers = pd.DataFrame(rows, columns=["q_id", "answer"])
 
     df_model_answers.to_csv(
-        os.path.join(DATA_DIR, out_dir, f"gpt3_5_grade_answers_prompt{PROMPT_IDX}_0shot_a_{student_level}.csv"),
+        os.path.join(DATA_DIR, OUTPUT_DIR, folder_name, f"gpt3_5_grade_answers_prompt{PROMPT_IDX}_0shot_a_{student_level}.csv"),
         index=False
     )
