47057418c78e065877f79bd0050e9379a613e806
Luca Benedetto <luca.benedetto93@gmail.com>
2023-11-03 10:49:59 +0000

    refactor constants and add new ones

--- constants.py
+++ constants.py
@@ -1,4 +1,10 @@
 RACE = 'race_pp'
 ARC = 'arc'
 
+IS_READING_QUESTION = {
+    RACE: True,
+    ARC: False,
+}
+
 DATA_DIR = '/home/luca/projects/_project_data/unsupervised_qdet'
+OUTPUT_DIR = 'output'
