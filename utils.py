4fdb67a8d882a2132dc3c443397041462b43c0a2
Luca Benedetto <luca.benedetto93@gmail.com>
2023-11-03 10:50:47 +0000

    moved shared utils to the utils.py file and add new prompt to list of system settings

--- utils.py
+++ utils.py
@@ -1,3 +1,21 @@
+import json
+import os
+from typing import Union
+
+import pandas as pd
+
+from constants import RACE, ARC, DATA_DIR
+
+
+def get_dataset(dataset_name: str, num_questions_per_difficulty_level: int = 50) -> pd.DataFrame:
+    if dataset_name == RACE and num_questions_per_difficulty_level == 50:
+        return pd.read_csv(os.path.join(DATA_DIR, "processed/race_pp_test_50q_per_diff.csv"))
+    elif dataset_name == ARC and num_questions_per_difficulty_level == 50:
+        return pd.read_csv(os.path.join(DATA_DIR, "processed/arc_test_50q_per_diff.csv"))
+    else:
+        raise NotImplementedError()
+
+
 def get_student_levels_from_prompt_idx(prompt_idx):
     # thw two standard approaches with numbers in chars, these (especially 5 levels) are the ones used the most.
     five_levels_char = ['one', 'two', 'three', 'four', 'five']
@@ -32,8 +50,15 @@ def get_student_levels_from_prompt_idx(prompt_idx):
     return student_levels_list
 
 
-def _build_system_message_from_params(prompt_idx, student_level):
+def build_system_message_from_params(prompt_idx, student_level):
     system_message = ''
+    if prompt_idx == 39:
+        system_message = f"""
+You are taking a science exam, and the questions in the exam have difficulty levels on a scale from one (very easy) to five (very difficult).
+You must assign a difficulty level to the given multiple choice question, and select the answer choice that a student of level {student_level} would pick.
+Provide only a JSON file with the following structure:
+{{"question level": "difficulty level of the question", "answer explanation": "the list of steps that the students of level {student_level} would follow to select the answer, including the misconceptions that might cause them to make mistakes", "index": "integer index of the answer chosen by a student of level {student_level}"}}
+"""
     if prompt_idx == 40:
         system_message = f"""
 You will be shown a multiple choice question from an English reading comprehension exam, and the questions in the exam have difficulty levels on a scale from one (very easy) to five (very difficult).
@@ -93,3 +118,31 @@ Provide only a JSON file with the following structure:
     else:
         raise NotImplementedError()
     return system_message
+
+
+def build_user_prompt_from_params(question, answers, is_reading_question, context=None) -> str:
+    if is_reading_question:
+        prompt = f"""
+Reading passage: "{context}"
+Question: "{question}"
+Options: "{answers}"
+"""
+    else:
+        prompt = f"""
+Question: "{question}"
+Options: "{answers}"
+"""
+    return prompt
+
+
+def validate_answer(answer: str) -> Union[dict, None]:
+    try:
+        answer_json = json.loads(answer)
+        index_str = str(answer_json['index'])
+        if not index_str.isdigit():
+            print("The index is not an integer.")
+            return None
+        return answer_json
+    except json.JSONDecodeError:
+        print("The answer is not a valid JSON string.")
+        return None
