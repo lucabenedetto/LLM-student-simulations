743a48d3bd03abf2cac32a721bc08d338e910bbf
Luca Benedetto <luca.benedetto93@gmail.com>
2023-11-06 12:56:30 +0000

    add first complete version of the script to plot the analysis of the results

--- script_eval_llm_responses.py
+++ script_eval_llm_responses.py
@@ -11,18 +11,21 @@ from utils import (
 from utils_plotting import (
     plot_accuracy_per_model,
     plot_accuracy_per_difficulty_per_model,
+    plot_accuracy_per_difficulty_for_different_role_played_levels,
+    plot_correlation_between_difficulty_and_qa_correctness,
 )
 from constants import (
     DIFFICULTY_LEVELS,
     RACE,
     ARC,
+    OUTPUT_DATA_DIR,
 )
 
 
-DATASET = ARC
-PROMPT_IDX = 39
+DATASET = RACE
+PROMPT_IDX = 40
+data_path = os.path.join(OUTPUT_DATA_DIR, f'gpt_responses_{DATASET}')
 
-data_path = "data/output/gpt_responses_arc"  # TODO
 student_levels = get_student_levels_from_prompt_idx(PROMPT_IDX)
 filenames = [f"gpt3_5_grade_answers_prompt{PROMPT_IDX}_0shot_a_{student_level}.csv" for student_level in student_levels]
 filepaths = [os.path.join(data_path, filename) for filename in filenames]
@@ -46,3 +49,8 @@ plot_accuracy_per_model(avg_accuracy_per_model, student_levels, DATASET, PROMPT_
 # accuracy per model per (true) difficulty level (i.e. grade)
 plot_accuracy_per_difficulty_per_model(avg_accuracy_per_grade_per_model, DATASET, PROMPT_IDX)
 
+# accuracy per different grades when role-playing different levels
+plot_accuracy_per_difficulty_for_different_role_played_levels(avg_accuracy_per_grade_per_model, student_levels, DATASET, PROMPT_IDX)
+
+# correlation between grade (difficulty) and QA correctness
+plot_correlation_between_difficulty_and_qa_correctness(correctness_per_model, difficulty_dict, DATASET, PROMPT_IDX)
