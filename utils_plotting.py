bcf6b788f0753eeaae7856987533cdaebea5477e
Luca Benedetto <luca.benedetto93@gmail.com>
2023-11-06 12:09:35 +0000

    clean formatting of method plot_accuracy_per_difficulty_per_model

--- utils_plotting.py
+++ utils_plotting.py
@@ -14,7 +14,7 @@ def plot_accuracy_per_model(average_accuracy_per_model, role_played_levels, data
     ax.bar(range(n_role_played_levels), average_accuracy_per_model)
     ax.set_ylim(0, 1.0)
     ax.set_yticks(np.arange(0.0, 1.0, 0.1))
-    ax.grid(axis='y')
+    ax.grid(alpha=0.5, axis='y')
     ax.set_ylabel('QA accuracy')
     ax.set_xlabel('Role-played level')
     ax.set_xticks(range(n_role_played_levels))
@@ -23,7 +23,7 @@ def plot_accuracy_per_model(average_accuracy_per_model, role_played_levels, data
     plt.show()
 
 
-def plot_accuracy_per_difficulty_per_model(avg_accuracy_per_grade_per_model):
+def plot_accuracy_per_difficulty_per_model(avg_accuracy_per_grade_per_model, dataset_name, prompt_idx):
     difficulty_levels = list(avg_accuracy_per_grade_per_model.keys())
     n_role_played_levels = len(avg_accuracy_per_grade_per_model[difficulty_levels[0]])
     for difficulty in difficulty_levels[1:]:
@@ -31,10 +31,10 @@ def plot_accuracy_per_difficulty_per_model(avg_accuracy_per_grade_per_model):
             print("WARNING!")  # TODO add raise error instead of this
     fig, ax = plt.subplots(1, len(difficulty_levels), sharey='all')
     for idx, grade in enumerate(difficulty_levels):
-        ax[idx].grid(alpha=0.5)
+        ax[idx].grid(alpha=0.5, axis='y')
         ax[idx].bar(range(n_role_played_levels), avg_accuracy_per_grade_per_model[grade], color=COLORS[idx])
         ax[idx].plot([0, n_role_played_levels-1], [np.mean(avg_accuracy_per_grade_per_model[grade])] * 2, c='k')
-        ax[idx].set_title(f'Grade: {grade}')
+        ax[idx].set_title(f'{dataset_name} | prompt {prompt_idx}\n Diff. level {grade} ')
         ax[idx].set_xlabel('Role-played level')
         ax[idx].set_ylabel('QA accuracy')
     plt.show()
