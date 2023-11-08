70a5c66fa9e77f6887fda871588add8294b3dc64
Luca Benedetto <luca.benedetto93@gmail.com>
2023-11-08 22:05:35 +0000

    add prompt 50

--- utils.py
+++ utils.py
@@ -122,6 +122,8 @@ def get_student_levels_from_prompt_idx(prompt_idx):
     # IELTS levels
     ielts_levels = ['4', '4.5', '5', '5.5', '6', '6.5', '7', '7.5', '8', '9']
     ielts_levels_2 = ['4', '5', '6', '7', '8', '9']
+    ielts_levels_with_def = ['4 (Limited test taker)', '5 (Modest test taker)', '6 (Competent test taker)',
+                             '7 (Good test taker)', '8 (Very good test taker)', '9 (Expert test taker)']
     # CEFR levels
     cefr_levels = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
     # TOEFL levels
@@ -142,6 +144,8 @@ def get_student_levels_from_prompt_idx(prompt_idx):
         return ielts_levels_2
     if prompt_idx in {49}:
         return rounded_toefl_levels
+    if prompt_idx in {50}:
+        return ielts_levels_with_def
     raise NotImplementedError()
 
 
@@ -181,7 +185,7 @@ You must assign a difficulty level to the given multiple choice question, and se
 Provide only a JSON file with the following structure:
 {{"question level": "difficulty level of the question", "answer explanation": "the list of steps that the students of CEFR level {student_level} would follow to select the answer, including the misconceptions that might cause them to make mistakes", "index": "integer index of the answer chosen by a student of CEFR level {student_level}"}}        
 """
-    if prompt_idx == 45:
+    if prompt_idx == 45 or prompt_idx == 50:
         return f"""
 You will be shown a multiple choice question from an English reading comprehension exam, and the questions in the exam have difficulty levels on a scale from one (very easy) to five (very difficult).
 You must assign a difficulty level to the given multiple choice question, and select the answer choice that a student of IELTS level {student_level} would pick.
