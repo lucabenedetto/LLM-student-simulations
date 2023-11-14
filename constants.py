5fba8e9ed16e9531fa77fe751df1af460d1d59a3
Luca Benedetto <luca.benedetto93@gmail.com>
2023-11-14 15:37:24 +0000

    add constants for CUPA data

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 RACE = 'race_pp'
 ARC = 'arc'
+CUPA = 'cupa'
 
 LLAMA2_7B = 'llama2_7b'
 LLAMA2_13B = 'llama2_13b'
@@ -12,6 +13,7 @@ IS_READING_QUESTION = {
 DIFFICULTY_LEVELS = {
     RACE: [0, 1, 2],
     ARC: [3, 4, 5, 6, 7, 8, 9],
+    CUPA: [],
 }
 
 INPUT_DATA_DIR = 'data/input'
