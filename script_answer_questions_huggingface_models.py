bbd3948e1dfdc5be45e964e3a6669dbaaf8fca6d
Luca Benedetto <luca.benedetto93@gmail.com>
2023-11-27 16:18:20 +0000

    update code for running vicuna

--- script_answer_questions_huggingface_models.py
+++ script_answer_questions_huggingface_models.py
@@ -36,10 +36,6 @@ def main():
     pipeline = transformers.pipeline(
         "text-generation",
         model=model,
-        tokenizer=tokenizer,  # This is tmp, to check
-        max_length=MAX_LENGTH,  # This is tmp, to check
-        temperature=0.7,  # This is tmp, to check
-        top_p=1,  # This is tmp, to check
         # repetition_penalty=1.15,  # This is tmp, to check
         # torch_dtype=torch.float16,  # This was here but removed for the time being.
         # device_map="auto",  # This was here but removed for the time being.
