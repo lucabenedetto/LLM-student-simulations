201ee4ecf2b586ca94e0f4f491ecfea4ec1ed3ff
Luca Benedetto <luca.benedetto93@gmail.com>
2023-11-27 16:24:11 +0000

    change params to fix warnings

--- utils_huggingface_models.py
+++ utils_huggingface_models.py
@@ -84,8 +84,6 @@ def prepare_answers_dict_huggingface_model(
     sequences = pipeline(
         df_questions['input_prompt'].values.tolist(),
         # do_sample=True,  # This was here but removed for the time being.
-        # temperature=0.7,  # This is tmp, to check
-        # top_p=1,  # This is tmp, to check
         # top_k=10,  # This was here but removed for the time being.
         # num_return_sequences=num_return_sequences,  # This was here but removed for the time being.
         return_full_text=return_full_text,
