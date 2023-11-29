53fd391fcc2521f6d6e43728c4d23ac8a8fdf834
giovanni.aradelli <giovanni.aradelli@cloudacademy.com>
2023-11-29 16:56:42 +0100

    feat: add gpt4 model

--- script_answer_questions_gpt.py
+++ script_answer_questions_gpt.py
@@ -12,6 +12,7 @@ from constants import (
     OUTPUT_DATA_DIR,
     GPT_3_5,
     GPT_3_5_1106,
+    GPT_4_1106
 )
 
 DATASET = RACE
@@ -30,22 +31,15 @@ def main():
     df_items = get_dataset(DATASET, 50)
     is_reading_question = IS_READING_QUESTION[DATASET]
     folder_name = f'{MODEL}_responses_{DATASET}'
+    if not os.path.exists(os.path.join(OUTPUT_DATA_DIR, folder_name)):
+        os.makedirs(os.path.join(OUTPUT_DATA_DIR, folder_name))
     model = get_gpt_model(MODEL)
 
     for idx, student_level in enumerate(st_levels):
         print(f"-- Doing idx {idx}, student level {student_level}")
         answers_dict = prepare_answers_dict_gpt(df_items, model, student_level, is_reading_question=is_reading_question, prompt_idx=PROMPT_IDX)
 
-        rows = []
-        for item, value in answers_dict.items():
-            row = []
-            row.append(item)
-            row.append(value)
-            rows.append(row)
-
-        # In older experiments, for GPT I didn't save the raw answer but only the processed one.
-        # df_model_answers = pd.DataFrame(rows, columns=["q_id", "answer"])
-        df_model_answers = pd.DataFrame(rows, columns=['q_id', 'raw_answer'])
+        df_model_answers = pd.DataFrame([(item, value) for item, value in answers_dict.items()], columns=['q_id', 'raw_answer'])
         df_model_answers['answer'] = df_model_answers.apply(lambda r: validate_answer(r['raw_answer']), axis=1)
 
         # the 1+idx is needed for backward compatibility with files written with a previous script.
